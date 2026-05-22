3a2f19fca41d8609b59cbacdf0183cd2164b5a68
Mark <mark@remarkablemark.org>
2026-05-22 01:39:05 -0400

    chore: load level from ?level= querystring

--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -1,5 +1,9 @@
 import { SCENE } from '../constants'
+import { LEVELS } from '../levels'
 
 scene(SCENE.PRELOAD, () => {
-  go(SCENE.GAME, 0)
+  const params = new URLSearchParams(window.location.search)
+  const raw = parseInt(params.get('level') ?? '1', 10)
+  const levelIndex = Math.min(Math.max(raw - 1, 0), LEVELS.length - 1)
+  go(SCENE.GAME, levelIndex)
 })
